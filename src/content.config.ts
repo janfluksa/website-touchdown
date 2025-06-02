d1d6c4c486fc66d0e41ae625642f0a917c0c1d93
Jan Fluksa <janfluksa@outlook.com>
2025-06-02 11:43:16 +0200

    Doplneni sekci

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,10 +1,12 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
+
 const services = defineCollection({
 loader: glob({ base: './src/collections/services', pattern: '**/*.{md,mdx}' }),
   schema: z.object({
     title: z.string(),
+    order: z.number(),
     link: z.string().url(),
     linkLabel: z.string(),
     image: z.string(),
