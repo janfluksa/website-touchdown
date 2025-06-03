2d47eb0d28dc4f51a7dd5eb790508fd614987987
Jan Fluksa <janfluksa@outlook.com>
2025-06-03 23:54:28 +0200

    styly

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,7 +1,7 @@
 
 export default new Map([
 ["src/collections/services/executive-search.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2Fservices%2Fexecutive-search.mdx&astroContentModuleFlag=true")],
-["src/collections/services/leadership-advisory.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2Fservices%2Fleadership-advisory.mdx&astroContentModuleFlag=true")],
 ["src/collections/services/talent-audit.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2Fservices%2Ftalent-audit.mdx&astroContentModuleFlag=true")],
-["src/collections/services/organizational-design.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2Fservices%2Forganizational-design.mdx&astroContentModuleFlag=true")]]);
+["src/collections/services/organizational-design.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2Fservices%2Forganizational-design.mdx&astroContentModuleFlag=true")],
+["src/collections/services/leadership-advisory.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2Fservices%2Fleadership-advisory.mdx&astroContentModuleFlag=true")]]);
 		
