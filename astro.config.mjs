9def4edc8862451a8d686bd31d402f7434f19154
Jan Fluksa <janfluksa@outlook.com>
2025-06-12 11:46:15 +0200

    rebuild static

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,10 @@ import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 
-import node from '@astrojs/node';
+const isDev = process.env.NODE_ENV !== 'production';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), markdoc(), keystatic()],
-
-  adapter: node({
-    mode: 'standalone',
-  }),
+  // integrations: [react(), markdoc(), keystatic()],
+  integrations: [react(), markdoc(), isDev && keystatic() ].filter(Boolean),
 });
